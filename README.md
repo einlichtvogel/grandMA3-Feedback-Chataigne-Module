dc9910f559936bddd6fa1431adda25fd60485d1e
Luca Heß <development@luca-hess.de>
2025-04-18 21:54:52 +0200

    init and first testing version

---
+++ README.md
@@ -0,0 +1,11 @@
+# grandMA3-Feedback-Chataigne-Module
+
+A Chataigne module to receive feedback from grandMA3 via OSC and an LUA plugin.
+
+## Features
+
+- Receive feedback from grandMA3 for:
+  - Executor (Normal & XKeys)
+    - Executor Color
+    - Button State
+
---
+++ grandMA3-Feedback.js
@@ -0,0 +1,114 @@
+var wingArray = [
+  { start: 1, end: 15 }, //wing 1
+  { start: 16, end: 30 }, //wing 2
+  { start: 31, end: 45 }, //wing 3
+  { start: 46, end: 60 }, //wing 4
+  { start: 61, end: 75 }, //wing 5
+];
+
+function formatNumber(num) {
+  if (num < 10) {
+    return '0' + num;  // Führende Null hinzufügen, wenn die Zahl kleiner als 10 ist
+  }
+  return num;  // Andernfalls die Zahl als String zurückgeben
+}
+
+function getWingNumberFromButtonNumber(execNumber) {
+  for (var i = 0; i < wingArray.length; i++) {
+    if (execNumber >= formatNumber(wingArray[i].start) && execNumber <= formatNumber(wingArray[i].end)) {
+      return i + 1; // Return the wing number (1-based index)
+    }
+  }
+
+  return -1; // Return -1 if no valid wing number is found
+}
+
+function init() {
+  script.log("Custom module init");
+
+  var executors = local.values.addContainer("Executors");
+  executors.setCollapsed(false);
+
+  //Create containers for each wing
+  for (i = 1; i <= wingArray.length; i++) {
+    var wing = executors.addContainer("Wing " + i);
+    wing.setCollapsed(true);
+
+    //Create containers for each row in the wing
+    for (j = 1; j <= 4; j++) {
+      var rowButtons = wing.addContainer("Row " + j + "00");
+      rowButtons.setCollapsed(true);
+
+      //Create buttons for each row
+      for (f = wingArray[i-1]["start"]; f <= wingArray[i-1]["end"]; f++) {
+        var buttonID = j + "" + formatNumber(f);
+        var btn = rowButtons.addContainer("Button " + buttonID);
+        btn.setCollapsed(true);
+        btn.addColorParameter("Color", "Color for button " + buttonID, 0x000000FF);
+        btn.addStringParameter("Color String", "Color for button " + buttonID + "as string", "255;255;255;255");
+        btn.addBoolParameter("Status", "Button " + buttonID + " on/off", false);
+      }
+    }
+  }
+
+  var xkeys = executors.addContainer("xKeys");
+  xkeys.setCollapsed(true);
+
+  //Create containers for each row in the x-keys
+  for (j = 1; j <= 2; j++) {
+    var rowButtons = xkeys.addContainer("Row " + j + "00");
+    rowButtons.setCollapsed(true);
+
+    //Create buttons for each row
+    for (f = 1; f <= 8; f++) {
+      var buttonID = j + "9" + f;
+      var btn = rowButtons.addContainer("Button " + buttonID);
+      btn.setCollapsed(true);
+      btn.addColorParameter("Color", "Color for button " + buttonID, 0x000000FF);
+      btn.addStringParameter("Color String", "Color for button " + buttonID + "as string", "255;255;255;255");
+      btn.addBoolParameter("Status", "Button " + buttonID + " on/off", false);
+    }
+  }
+}
+
+function oscEvent(address, args) {
+  script.log("OSC event received: " + address + ", args: " + args);
+
+  // Check if the address starts with "/Exec"
+  if (address.startsWith("/Exec")) {
+
+
+    var execNumber = address.split("/")[1].split("c")[1];
+    var rowNumber = execNumber.charAt(0);
+    var buttonNumber = execNumber.charAt(1) + "" + execNumber.charAt(2);
+    var type = address.split("/")[2];
+
+    var btn;
+
+    if(parseInt(buttonNumber) >= 91 && (parseInt(buttonNumber) <= 98)) {
+      // X-Keys
+      btn = local.values.executors["xKeys"]["row" + rowNumber + "00"]["button" + execNumber];
+    }else{
+      btn = local.values.executors["wing" + getWingNumberFromButtonNumber(buttonNumber)]["row" + rowNumber + "00"]["button" + execNumber];
+    }
+
+
+
+    if(type === "Button") {
+      // Set the status parameter of the button
+      btn.status.set(args[0] === "On");
+    }
+
+    if(type === "Color") {
+      // Set the color parameter of the button
+      btn.colorString.set(args[0]);
+      var spl = args[0].split(";");
+
+      var newCol = [1,1,1,1];
+      for(var i=0;i<spl.length-1;i++) newCol[i] = parseInt(spl[i])/255.0;
+
+      btn.color.set(newCol);
+    }
+  }
+
+}
