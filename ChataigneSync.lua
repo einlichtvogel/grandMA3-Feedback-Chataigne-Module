334393b9e70a4cb64dd3271fa61b97ddb8856520
Luca Heß <development@luca-hess.de>
2025-05-19 20:16:24 +0200

    added version

--- ChataigneSync.lua
+++ ChataigneSync.lua
@@ -11,6 +11,7 @@
 -- GNU General Public License for more details.
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <https://www.gnu.org/licenses/>.
+-- v3.0.0.1
 
 
 local executorsToWatchCurrentPage = {}
